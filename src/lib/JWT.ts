e0bf30eb50242f11514929d69c7b268a5af410cb
Pavel <Papaskaaaa@gmail.com>
2025-06-20 14:42:46 +0300

    feat: add default values for secret and expiresIn in generateJWT function

--- src/lib/JWT.ts
+++ src/lib/JWT.ts
@@ -1,18 +1,19 @@
 import { JWTPayload, SignJWT } from 'jose';
+import { REFRESH_TOKEN_EXPIRES, REFRESH_TOKEN_SECRET } from '@/domain/constants/JWT';
 
 /**
  * Generates a JWT using the jose library.
  * @param payload - The payload to include in the token.
  * @param secret - The secret string used for signing.
+ * @param expiresIn - Token expiration time (e.g., '1h', '10m')
  * @param alg - The signing algorithm (e.g., 'HS256', 'RS256'). Default is 'HS256'.
- * @param expiresIn - Token expiration time (e.g., '1h', '10m'). Default is '1h'.
  *
  * @returns Promise<string> - The generated JWT as a string.
  */
 export async function generateJWT(
   payload: JWTPayload,
-  secret: string,
-  expiresIn: string = '1h',
+  secret: string = REFRESH_TOKEN_SECRET,
+  expiresIn: string = REFRESH_TOKEN_EXPIRES,
   alg: string = 'HS256',
 ): Promise<string> {
   const encoder = new TextEncoder();
