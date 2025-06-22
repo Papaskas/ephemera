ba0f8b916b24844375fef163a8045417d9c96c97
Pavel <Papaskaaaa@gmail.com>
2025-06-22 16:06:54 +0300

    refactor: rename and update all constants

--- src/domain/constants/cookies.ts
+++ src/domain/constants/cookies.ts
@@ -1,9 +1,19 @@
 export const COOKIES = {
-  USER: {
-    NAME: 'publicId',
+  REFRESH_TOKEN: {
+    NAME: 'refresh-token',
     MAX_AGE: 60 * 60 * 24 * 365,
     PATH: '/',
-    HTTP_ONLY: true,
+    HTTP_ONLY: false,
     SAME_SITE: 'lax',
+    SECURE: process.env.NODE_ENV === "production",
+  },
+
+  ACCESS_TOKEN: {
+    NAME: 'access-token',
+    MAX_AGE: 60 * 15,
+    PATH: '/',
+    HTTP_ONLY: false,
+    SAME_SITE: 'lax',
+    SECURE: process.env.NODE_ENV === "production",
   },
 } as const;
---
+++ src/domain/constants/usernames.ts
@@ -0,0 +1,4 @@
+export const USERNAMES = {
+  ADJECTIVES: ['Cool', 'Fast', 'Silent', 'Brave', 'Lucky', 'Smart', 'Mighty', 'Clever', 'Wild', 'Happy'],
+  NOUNS: ['Tiger', 'Falcon', 'Lion', 'Wolf', 'Eagle', 'Shark', 'Panther', 'Bear', 'Fox', 'Hawk'],
+} as const;
