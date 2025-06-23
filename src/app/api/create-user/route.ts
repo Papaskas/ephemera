fd4f16ff38b3e9826ab2ce360f38db705c454006
Pavel <Papaskaaaa@gmail.com>
2025-06-23 14:52:38 +0300

    chore: update logic create-user

--- src/app/api/create-user/route.ts
+++ src/app/api/create-user/route.ts
@@ -1,20 +1,46 @@
 import { NextResponse } from 'next/server';
 import { createUser } from '@/functions/createUser';
-import { generateJWT } from '@/functions/generateJWT';
-import { REFRESH_TOKEN_EXPIRES, REFRESH_TOKEN_SECRET } from '@/domain/constants/JWT';
+import { generateJWT } from '@/functions/JWT';
+import {
+  ACCESS_TOKEN_EXPIRES,
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRES,
+  REFRESH_TOKEN_SECRET
+} from '@/domain/constants/JWT';
 import { randomUUID } from 'node:crypto';
+import { COOKIES } from '@/domain/constants/cookies';
 
 export async function POST() {
   const createdAt = new Date()
   const userId = randomUUID()
 
   const refreshToken = await generateJWT({ userId, createdAt }, REFRESH_TOKEN_SECRET, REFRESH_TOKEN_EXPIRES)
+  const accessToken = await generateJWT({ userId, createdAt }, ACCESS_TOKEN_SECRET, ACCESS_TOKEN_EXPIRES)
 
   await createUser(
     refreshToken,
+    accessToken,
     createdAt,
     userId
   )
 
-  return NextResponse.json({ refreshToken })
+  const response = NextResponse.next()
+
+  response.cookies.set(COOKIES.REFRESH_TOKEN.NAME, refreshToken, {
+    httpOnly: COOKIES.REFRESH_TOKEN.HTTP_ONLY,
+    path: COOKIES.REFRESH_TOKEN.PATH,
+    sameSite: COOKIES.REFRESH_TOKEN.SAME_SITE,
+    secure: COOKIES.REFRESH_TOKEN.SECURE,
+    maxAge: COOKIES.REFRESH_TOKEN.MAX_AGE
+  })
+
+  response.cookies.set(COOKIES.ACCESS_TOKEN.NAME, accessToken, {
+    httpOnly: COOKIES.ACCESS_TOKEN.HTTP_ONLY,
+    path: COOKIES.ACCESS_TOKEN.PATH,
+    sameSite: COOKIES.ACCESS_TOKEN.SAME_SITE,
+    secure: COOKIES.ACCESS_TOKEN.SECURE,
+    maxAge: COOKIES.ACCESS_TOKEN.MAX_AGE
+  })
+
+  return response
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,25 +2,12 @@ import { NextRequest, NextResponse } from 'next/server';
 import { NextConfig } from 'next';
 import { COOKIES } from '@/domain/constants/cookies';
 
-type Res = {
-  refreshToken: string
-}
-
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
 
   if(!request.cookies.has(COOKIES.REFRESH_TOKEN.NAME)) {
-    const apiUrl = `${request.nextUrl.origin}/api/create-user`;
-    const apiResponse = await fetch(apiUrl, { method: 'POST' });
-    const data = await apiResponse.json() as Res;
-
-    response.cookies.set(COOKIES.REFRESH_TOKEN.NAME, data.refreshToken, {
-      httpOnly: COOKIES.REFRESH_TOKEN.HTTP_ONLY,
-      path: COOKIES.REFRESH_TOKEN.PATH,
-      sameSite: COOKIES.REFRESH_TOKEN.SAME_SITE,
-      secure: COOKIES.REFRESH_TOKEN.SECURE,
-      maxAge: COOKIES.REFRESH_TOKEN.MAX_AGE
-    });
+    const apiUrl = `${request.nextUrl.origin}/api/create-user`
+    await fetch(apiUrl, { method: 'POST' })
   }
 
   return response
