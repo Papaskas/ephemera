f0a8fca69bb925121b38ed63dbaf8f0f37343b83
Pavel <Papaskaaaa@gmail.com>
2025-06-24 13:18:33 +0300

    chore: delete constans/usernames

--- src/domain/constants/usernames.ts
+++
@@ -1,4 +0,0 @@
-export const USERNAMES = {
-  ADJECTIVES: ['Cool', 'Fast', 'Silent', 'Brave', 'Lucky', 'Smart', 'Mighty', 'Clever', 'Wild', 'Happy'],
-  NOUNS: ['Tiger', 'Falcon', 'Lion', 'Wolf', 'Eagle', 'Shark', 'Panther', 'Bear', 'Fox', 'Hawk'],
-} as const;
